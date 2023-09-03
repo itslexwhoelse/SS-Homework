e78829781f96abad50461526cbbd2f8bbbd90b3f
itslexwhoelse <deckeralexander41@gmail.com>
2023-09-03 19:16:50 -0400

    homeworky

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let blurredThing = document.getElementById("blurredThing");
+//flollowed this https://www.w3schools.com/tags/av_prop_playbackrate.asp#:~:text=Property%20Values&text=1.0%20is%20normal%20speed,1.0%20is%20backwards%2C%20normal%20speed sorta to figure this out
+function setPlaybackSpeed() {
+  blurredThing.playbackRate=0.05;
+}
